6ce8d8328b00dd0d6fde8b9dec5391dc83985a21
Suman Pokhrel <spokhrel196@gmail.com>
2023-12-03 21:55:24 +0545

    added controller to get single suer

--- controllers/userController.go
+++ controllers/userController.go
@@ -1,9 +1,16 @@
 package controllers
 
 import (
+	"context"
+	"net/http"
+	"time"
+
 	"github.com/Suman196pokhrel/go-jwt-auth/database"
+	"github.com/Suman196pokhrel/go-jwt-auth/helpers"
+	"github.com/Suman196pokhrel/go-jwt-auth/models"
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -30,6 +37,26 @@ func GetUsers(c *gin.Context) {
 
 }
 
-func GetUser(c *gin.Context) {
+func GetUser() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		userId := ctx.Param("user_id")
+
+		if err := helpers.MatchUserTypeToUid(ctx, userId); err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		var c, cancle = context.WithTimeout(context.Background(), 100*time.Second)
+		var user models.User
+		err := userCollection.FindOne(c, bson.M{"user_id": userId}).Decode(&user)
+		defer cancle()
+
+		if err != nil {
+			ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+
+		ctx.JSON(http.StatusOK, user)
+	}
 
 }
