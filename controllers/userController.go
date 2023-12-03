e34e14e8d4a959ed74ad4c3b6871c43de6221f58
Suman Pokhrel <spokhrel196@gmail.com>
2023-12-03 20:45:15 +0545

    added function definations for controllers

--- controllers/userController.go
+++ controllers/userController.go
@@ -1,1 +1,35 @@
 package controllers
+
+import (
+	"github.com/Suman196pokhrel/go-jwt-auth/database"
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+var userCollection *mongo.Collection = database.OpenCollection(database.Client, "user")
+var validate = validator.New()
+
+func HashPassword() {
+
+}
+
+func VerifyPassword() {
+
+}
+
+func Signup(c *gin.Context) {
+
+}
+
+func Login(c *gin.Context) {
+
+}
+
+func GetUsers(c *gin.Context) {
+
+}
+
+func GetUser(c *gin.Context) {
+
+}
